d5b97947dbbdf9b3fd06a06f28d19e5ede50106c
Ankoor Patel <ankoor.p@gmail.com>
2013-06-18 15:43:01 +0100

    written a sample workflow for the calculator

--- calc-it.rb
+++ calc-it.rb
@@ -1,6 +1,7 @@
 # Function definitions first
 def menu
   # Clear the screen, and present the user with a menu
+  sleep(5)
   puts `clear`
   puts "***CalcIt***"
   print "(b)asic, (a)dvanced, or (q)uit: "
@@ -23,6 +24,8 @@ end
 
 def get_numbers
   # ask the user for numbers to work on
+  print "Give me NUMBERS!!:"
+  numbers = gets.chomp.split.map  { |s| s.to_f }
 end
 
 def add
@@ -56,6 +59,21 @@ while response != 'q'
     basic_calc
   when 'a'
     advanced_calc
+  end
+
+  case operation
+  when a
+    add(number[0],numbers[1])
+  when s
+    subtract(number[0],numbers[1])
+  when m
+    multiply(number[0],numbers[1])
+  when d
+    divide(number[0],numbers[1])
+  when p
+    square(number[0],numbers[1])
+  when q
+    sqrt(number[0],numbers[1])
   end
 
   response = menu
