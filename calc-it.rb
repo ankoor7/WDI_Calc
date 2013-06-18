ad616ff264c4c5b3f450711d780902d7a2198b8d
Ankoor Patel <ankoor.p@gmail.com>
2013-06-18 15:57:41 +0100

    calls get_numbers

--- calc-it.rb
+++ calc-it.rb
@@ -72,13 +72,13 @@ numbers  = get_numbers
     add(numbers[0],numbers[1])
   when "s"
     subtract(numbers[0],numbers[1])
-  when m
+  when "m"
     multiply(numbers[0],numbers[1])
-  when d
+  when "d"
     divide(numbers[0],numbers[1])
-  when p
+  when "p"
     square(numbers[0],numbers[1])
-  when q
+  when "q"
     sqrt(numbers[0],numbers[1])
   end
 
